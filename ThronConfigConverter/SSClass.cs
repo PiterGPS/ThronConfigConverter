ba88bdb3547a047ab94a627cef98ae5831d72519
Piter <piter554@rambler.ru>
2026-04-09 11:06:59 +0300

    Clear code

--- ThronConfigConverter/SSClass.cs
+++ ThronConfigConverter/SSClass.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
+﻿using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace ThronConfigConverter
